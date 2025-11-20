c9cb46a8d24ba212c0642cf51efa3bbf8af4ca38
Nias26 <genangeliludovico26@gmail.com>
2025-11-20 19:47:12 +0100

    zsh: update ex script

--- .scripts/ex.zsh
+++ .scripts/ex.zsh
@@ -6,23 +6,44 @@
 ex ()
 {
   if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2)   tar xjf $1   		;;
-      *.tar.gz)    tar xzf $1   		;;
-      *.bz2)       bunzip2 $1   		;;
-      *.rar)       unrar x $1     	;;
-      *.gz)        gunzip $1    		;;
-      *.tar)       tar xf $1    		;;
-      *.xz)        tar xf $1    		;;
-      *.tbz2)      tar xjf $1   		;;
-      *.tgz)       tar xzf $1   		;;
-      *.zip)       unzip $1 -d "$(basename $1 .zip)"	;;
-      *.Z)         uncompress $1 	  ;;
-      *.7z)        7z x $1      		;;
-      *.zst)       zstd -d $1       ;;
-      *)           echo "'$1' cannot be extracted via ex()" ;;
+    local file="$1"
+    local base
+
+    case "$file" in
+      *.tar.bz2|*.tbz2)  base="${file%.tar.bz2}"; base="${base%.tbz2}" ;;
+      *.tar.gz|*.tgz)    base="${file%.tar.gz}";  base="${base%.tgz}" ;;
+      *.tar.xz)          base="${file%.tar.xz}" ;;
+      *.zip)             base="${file%.zip}" ;;
+      *.rar)             base="${file%.rar}" ;;
+      *.7z)              base="${file%.7z}" ;;
+      *.zst)             base="${file%.zst}" ;;
+      *.bz2)             base="${file%.bz2}" ;;
+      *.gz)              base="${file%.gz}" ;;
+      *.Z)               base="${file%.Z}" ;;
+      *)                 base="${file%.*}" ;;
+    esac
+
+    mkdir -p "$base"
+
+    case "$file" in
+      *.tar.bz2|*.tbz2)  tar xjf "$file" -C "$base" ;;
+      *.tar.gz|*.tgz)    tar xzf "$file" -C "$base" ;;
+      *.tar.xz)          tar xJf "$file" -C "$base" ;;
+      *.tar)             tar xf "$file" -C "$base" ;;
+      *.zip)             unzip -d "$base" "$file" ;;
+      *.rar)             unrar x "$file" "$base" ;;
+      *.7z)              7z x "$file" "-o$base" ;;
+      *.bz2)             bunzip2 -c "$file" > "$base/$(basename "$base")" ;;
+      *.gz)              gunzip -c "$file" > "$base/$(basename "$base")" ;;
+      *.Z)               uncompress -c "$file" > "$base/$(basename "$base")" ;;
+      *.zst)             zstd -d -c "$file" > "$base/$(basename "$base")" ;;
+      *)
+        echo "'$file' cannot be extracted via ex()"
+        return 1
+        ;;
     esac
   else
     echo "'$1' is not a valid file"
   fi
 }
+
