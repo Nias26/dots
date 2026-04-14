28c2fd1faba63f7326ce5b96f5e83177d4b82c1e
Nias26 <genangeliludovico26@gmail.com>
2026-04-14 22:50:29 +0200

    waybar: remove packages update (broken sudo)

--- .config/waybar/scripts/pkg-updates.sh
+++ .config/waybar/scripts/pkg-updates.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
+# TODO: Fix sudo password
 text="$USER"
 tooltip=""
 class="normal"
 
-updates=$(yay -Sy && yay -Qu 2>/dev/null | wc -l)
+#updates=$(yay -Sy && yay -Qu 2>/dev/null | wc -l)
+updates=0
 kernel=$(uname -a)
 packages="󰏗 ${updates} pending updates..."
 user=$(lslogins $USER | sed '/^$/q')
 
 if [ "$updates" -gt 0 ]; then
   tooltip="<span foreground='#33B1FF'>${kernel}</span>"$'\n'"<span foreground='#FF7EB6'>${packages}</span>"$'\n'"<span foreground='#42BE65'>${user}</span>"
-  if [ "$updates" -gt 60 ]; then
-    class="lots-of-packages"
-  fi
 else
   tooltip="<span foreground='#33B1FF'>${kernel}</span>"$'\n'"<span foreground='#42BE65'>${user}</span>"
 fi
