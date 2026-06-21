165ef6961656e556e0dbaff46868431887db17ea
Nias26 <genangeliludovico26@gmail.com>
2026-06-21 20:18:13 +0200

    hypr: discard use of uwsm with sddm

--- .config/hypr/hyprstart.lua
+++ .config/hypr/hyprstart.lua
@@ -1,11 +1,19 @@
 -- hyprland @ autostart
 -- Wiki: https://wiki.hypr.land/Configuring/Basics/Autostart/
 
-local uwsm = "uwsm app -- "
+-- local uwsm = "uwsm app -- "
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd(uwsm .. "awww-daemon")
-	hl.exec_cmd(uwsm .. "mpDris2 -j")
+	hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("~/.config/hypr/scripts/awww-manager")
+	hl.exec_cmd("mpDris2 -j")
+  hl.exec_cmd("udiskie -s")
+  hl.exec_cmd("hypridle")
+  hl.exec_cmd("mako")
+  hl.exec_cmd("~/.config/hypr/scripts/workspace-watcher")
+  hl.exec_cmd("~/.config/hypr/scripts/battery-monitor")
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("vicinae server")
 	-- hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
 	-- hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'")
 	-- hl.exec_cmd("gsettings set org.gnome.desktop.interface font-name 'Cantarell 11'")
